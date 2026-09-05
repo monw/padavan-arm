0784506386ebe4bc6beb365eefa965eb1c46b1da
monw <65690955+monw@users.noreply.github.com>
2026-09-05 23:27:42 +0800

    Add files via upload

---
+++ trunk/configs/boards/MT3000/board.h
@@ -0,0 +1,26 @@
+/* K2P */
+
+#define BOARD_PID                "MT3000"
+#define BOARD_NAME                "MT3000"
+#define BOARD_DESC                "The Customed GL-INET MT300 router"
+#define BOARD_VENDOR_NAME        "RAX"
+#define BOARD_VENDOR_URL        "http://www.phicomm.com/"
+#define BOARD_MODEL_URL                "http://www.phicomm.com/"
+#define BOARD_BOOT_TIME                30
+#define BOARD_FLASH_TIME        120
+
+#define BOARD_HAS_5G_11AC                1
+#define BOARD_HAS_5G_11AX                1
+#define BOARD_HAS_2G_11AX               1
+#define BOARD_NUM_ANT_5G_TX                2
+#define BOARD_NUM_ANT_5G_RX                2
+#define BOARD_NUM_ANT_2G_TX                2
+#define BOARD_NUM_ANT_2G_RX                2
+#define BOARD_HAS_EPHY_L1000        1
+#define BOARD_HAS_EPHY_W1000        1
+#define BOARD_NUM_ETH_LEDS                0
+#define BOARD_NUM_UPHY_USB3                1
+#define BOARD_USB_PORT_SWAP                0
+
+
+#define USE_IPV6 1
